2b6bfdf04398d8e247d2bfc2b4f2163fad431b9e
kpcurai <kristen@curai.com>
2024-01-25 16:50:50 -0600

    hitSlop

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -14,42 +14,9 @@ import { useState } from 'react'
 import { useLink } from 'solito/link'
 
 export function HomeScreen() {
-  const linkProps = useLink({
-    href: '/user/nate',
-  })
-
   return (
     <YStack f={1} jc="center" ai="center" p="$4" space>
-      <YStack space="$4" bc="$background">
-        <H1 ta="center">Welcome to Tamagui.</H1>
-        <Paragraph ta="center">
-          Here's a basic starter to show navigating from one screen to another. This screen uses the
-          same code on Next.js and React Native.
-        </Paragraph>
-
-        <Separator />
-        <Paragraph ta="center">
-          Made by{' '}
-          <Anchor color="$color12" href="https://twitter.com/natebirdman" target="_blank">
-            @natebirdman
-          </Anchor>
-          ,{' '}
-          <Anchor
-            color="$color12"
-            href="https://github.com/tamagui/tamagui"
-            target="_blank"
-            rel="noreferrer"
-          >
-            give it a ⭐️
-          </Anchor>
-        </Paragraph>
-      </YStack>
-
-      <XStack>
-        <Button {...linkProps}>Link to user</Button>
-      </XStack>
-
-      <SheetDemo />
+      <Button hitSlop={{ top: 10 }}>hitSlop</Button>
     </YStack>
   )
 }
